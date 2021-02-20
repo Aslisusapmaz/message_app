bbf50fb81b7348497f986f2bf7ed64d0afc1d54b
Aslısu Sapmaz <72272463+Aslisusapmaz@users.noreply.github.com>
2021-02-20 19:33:11 +0300

    adding post likes button

--- lib/main.dart
+++ lib/main.dart
@@ -57,7 +57,7 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Column(
         children: <Widget>[
 
-          Expanded(child: PostList()
+          Expanded(child: PostList(this.posts)
               ),
           Expanded(child: TextInputWidget(this.newPost)),
         ],
@@ -105,13 +105,47 @@ class _TextInputWidgetState extends State<TextInputWidget> {
 }
 
 class PostList extends StatefulWidget {
+  final List<Post> listItems;
+  PostList(this.listItems);
   @override
   _PostListState createState() => _PostListState();
 }
 
 class _PostListState extends State<PostList> {
+  void like(Function callback){
+    this.setState(() {
+      callback();
+    });
+
+  }
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView.builder(
+      itemCount: this.widget.listItems.length,
+      itemBuilder: (context,index){
+       var post=this.widget.listItems[index];
+       return Card(
+         child: Row(
+           children: <Widget>[
+             Expanded(child: ListTile(title:Text(post.body),
+             subtitle: Text(post.author),
+             )),
+             Row(
+               children: <Widget>[
+                 Container(child: Text(post.likes.toString(),
+                 style: TextStyle(fontSize: 20)
+                 ),
+                   padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                 ),
+               IconButton(icon: Icon(Icons.thumb_up),
+                   onPressed:()=>this.like( post.likePost),
+               )
+             ] ,)
+           ],
+         ),
+       );
+      },
+
+    );
   }
 }
